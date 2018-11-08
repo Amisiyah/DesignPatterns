8d4d14eab33e0b7111f65908dcab5daa33d3c23e
Amisiyah <bogdy.ponta@gmail.com>
2018-11-08 19:41:47 +0200

    implementation 4

---
+++ src/bookProject/Main.java
@@ -0,0 +1,46 @@
+package bookProject;
+
+public class Main {
+	public static void main(String[] args) {
+
+			Book mathBook = new Book("Base Math");
+			mathBook.add(new Paragraph("Thanks to my parents"));
+			Section cap1 = new Section("Basic Math");
+			cap1.add(new Paragraph("Bla Bla 2"));
+			Section cap11 = new Section("Set parameters");
+			cap1.add(cap11);
+			Section cap111 = new Section("Finish");
+			cap11.add(cap111);
+			mathBook.add(cap1);
+			mathBook.print();
+
+            long startTime = System.currentTimeMillis();
+            ImageProxy img1 = new ImageProxy("Pamela Anderson");
+            ImageProxy img2 = new ImageProxy("Kim Kardashian");
+            ImageProxy img3 = new ImageProxy("Kirby Griffin");
+            Section playboyS1 = new Section("Front Cover");
+            playboyS1.add(img1);
+            Section playboyS2 = new Section("Summer Girls");
+            playboyS2.add(img2);
+            playboyS2.add(img3);
+            Book playboy = new Book("Playboy");
+            playboy.add(playboyS1);
+            playboy.add(playboyS2);
+            long endTime = System.currentTimeMillis();
+            System.out.println("Creation of the content took " + (endTime - startTime) + " milliseconds");
+
+            startTime = System.currentTimeMillis();
+            playboyS1.print();
+            endTime = System.currentTimeMillis();
+
+            System.out.println("Printing of the section 1 took " + (endTime - startTime) + " milliseconds");
+
+            startTime = System.currentTimeMillis();
+            playboyS1.print();
+            endTime = System.currentTimeMillis();
+
+            System.out.println("Printing again the section 1 took " + (endTime - startTime) + " milliseconds");
+    }
+
+
+}
